f7a6d353391acac7afbde4a48d23b943796cf99a
Elosowaldo28 <osowal28@gmail.com>
2026-06-22 07:00:48 -0500

    crud de platos

--- backend/routes/platoRoutes.js
+++ backend/routes/platoRoutes.js
@@ -37,4 +37,49 @@ router.post('/platos', (req, res) => {
 
 });
 
+router.put('/platos/:id', (req, res) => {
+
+    const { nombre, precio, stock } = req.body;
+    const { id } = req.params;
+
+    const sql = `
+        UPDATE plato
+        SET nombre = ?, precio = ?, stock = ?
+        WHERE id_plato = ?
+    `;
+
+    db.query(sql, [nombre, precio, stock, id], (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json({
+            mensaje: 'Plato actualizado correctamente'
+        });
+
+    });
+
+});
+
+router.delete('/platos/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    const sql = 'DELETE FROM plato WHERE id_plato = ?';
+
+    db.query(sql, [id], (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json({
+            mensaje: 'Plato eliminado correctamente'
+        });
+
+    });
+
+});
+
 module.exports = router;
