362e753b407078c557707e282dc266068b78bc45
Elosowaldo28 <osowal28@gmail.com>
2026-06-22 07:39:05 -0500

    frontend desacoplado con API

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,21 @@
- 
+ async function cargarPlatos() {
+
+    const respuesta = await fetch('http://localhost:3000/api/platos');
+
+    const platos = await respuesta.json();
+
+    let html = '';
+
+    platos.forEach(plato => {
+
+        html += `
+            <li>
+                ${plato.nombre} - S/. ${plato.precio}
+            </li>
+        `;
+
+    });
+
+    document.getElementById('listaPlatos').innerHTML = html;
+
+}
