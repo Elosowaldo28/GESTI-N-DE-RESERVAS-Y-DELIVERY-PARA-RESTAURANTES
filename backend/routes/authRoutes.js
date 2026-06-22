9d46c366eb6e8e111b5cbfc621fb317f5a3f5d5e
Elosowaldo28 <osowal28@gmail.com>
2026-06-22 06:49:25 -0500

    Implementar inicio de sesion con JWT

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const jwt = require('jsonwebtoken');
 
 router.get('/register', (req, res) => {
     res.send('Ruta register funcionando');
@@ -20,6 +21,44 @@ router.post('/register', (req, res) => {
             mensaje: 'Usuario registrado correctamente'
         });
     });
+});
+
+router.post('/login', (req, res) => {
+
+    const { email, password } = req.body;
+
+    const sql = 'SELECT * FROM usuario WHERE email = ? AND password = ?';
+
+    db.query(sql, [email, password], (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        if (result.length === 0) {
+            return res.status(401).json({
+                mensaje: 'Credenciales incorrectas'
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: result[0].id_usuario,
+                rol: result[0].rol
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '1h'
+            }
+        );
+
+        res.json({
+            mensaje: 'Login exitoso',
+            token
+        });
+
+    });
+
 });
 
 module.exports = router;
