6b6ba5532770da39222f099d0852b1319b1009d2
Elosowaldo28 <osowal28@gmail.com>
2026-06-22 09:02:31 -0500

    Implementar mapa interactivo

--- frontend/app.js
+++ frontend/app.js
@@ -19,3 +19,41 @@
     document.getElementById('listaPlatos').innerHTML = html;
 
 }
+function obtenerUbicacion() {
+
+    navigator.geolocation.getCurrentPosition(
+
+    function(posicion) {
+
+    let latitud = posicion.coords.latitude;
+    let longitud = posicion.coords.longitude;
+
+    document.getElementById("coordenadas").innerHTML =
+        "Latitud: " + latitud +
+        "<br>Longitud: " + longitud +
+        "<br>Precisión: " + posicion.coords.accuracy + " metros";
+
+    var mapa = L.map('map').setView([latitud, longitud], 18);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19
+    }).addTo(mapa);
+
+    L.marker([latitud, longitud])
+        .addTo(mapa)
+        .bindPopup("Tu ubicación")
+        .openPopup();
+
+    },
+    function(error){
+    console.log(error);
+    },
+    {
+    enableHighAccuracy: true,
+    timeout: 10000,
+    maximumAge: 0
+    }
+
+);
+
+}
